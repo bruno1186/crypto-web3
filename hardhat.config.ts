ab0b664e64f96116d49199af786266875f8ad19d
Bruno <84094962+bruno1186@users.noreply.github.com>
2026-07-25 10:15:24 -0300

    Create hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: { enabled: true, runs: 200 },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL ?? '',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
